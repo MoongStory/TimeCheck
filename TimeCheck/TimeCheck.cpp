b470fb0b36d1a0b7191dee31ad7084c1dd40b5e4
곽문구 <moongstory@hotmail.com>
2023-02-25 18:40:34 +0900

    시간 출력 부분 Time 클래스 사용하도록 수정

--- TimeCheck/TimeCheck.cpp
+++ TimeCheck/TimeCheck.cpp
@@ -4,6 +4,7 @@
 #include <strsafe.h>
 
 #include "../../DebugView/DebugView/DebugView.h"
+#include "../../Time/Time/Time.h"
 
 MOONG::TIME_CHECK::TimeCheck::TimeCheck(const std::string delimiter, const std::string description) :
 	start_(0),
@@ -20,18 +21,15 @@ MOONG::TIME_CHECK::TimeCheck::TimeCheck(const std::string delimiter, const std::
 
 	start_ = time(NULL);
 
-	struct tm current_time;
-	localtime_s(&current_time, &start_);
-
 	if (!description.empty())
 	{
 		this->setDescription(description);
 
-		MOONG::DebugView::Print("시작 시간[%02d시 %02d분 %02d초] %s", current_time.tm_hour, current_time.tm_min, current_time.tm_sec, this->getDescription().c_str());
+		MOONG::DebugView::Print("시작 시간[%s] %s", MOONG::Time::get_current_time("HOUR(format:02)시 MINUTE(format:02)분 SECOND(format:02)초").c_str(), this->getDescription().c_str());
 	}
 	else
 	{
-		MOONG::DebugView::Print("시작 시간[%02d시 %02d분 %02d초]", current_time.tm_hour, current_time.tm_min, current_time.tm_sec);
+		MOONG::DebugView::Print("시작 시간[%s]", MOONG::Time::get_current_time("HOUR(format:02)시 MINUTE(format:02)분 SECOND(format:02)초").c_str());
 	}
 }
 
@@ -41,16 +39,13 @@ MOONG::TIME_CHECK::TimeCheck::~TimeCheck()
 {
 	end_ = time(NULL);
 
-	struct tm current_time;
-	localtime_s(&current_time, &end_);
-
 	if(this->getDescription().empty())
 	{
-		MOONG::DebugView::Print("종료 시간[%02d시 %02d분 %02d초] (%.3lf (Sec) 소요)", current_time.tm_hour, current_time.tm_min, current_time.tm_sec, (double)(this->end_ - this->start_));
+		MOONG::DebugView::Print("종료 시간[%s] (%.3lf (Sec) 소요)", MOONG::Time::get_current_time("HOUR(format:02)시 MINUTE(format:02)분 SECOND(format:02)초").c_str(), (double)(this->end_ - this->start_));
 	}
 	else
 	{
-		MOONG::DebugView::Print("종료 시간[%02d시 %02d분 %02d초] %s (%.3lf (Sec) 소요)", current_time.tm_hour, current_time.tm_min, current_time.tm_sec, this->getDescription().c_str(), (double)(this->end_ - this->start_));
+		MOONG::DebugView::Print("종료 시간[%s] %s (%.3lf (Sec) 소요)", MOONG::Time::get_current_time("HOUR(format:02)시 MINUTE(format:02)분 SECOND(format:02)초").c_str(), this->getDescription().c_str(), (double)(this->end_ - this->start_));
 	}
 }
 
